ffc1429a780289cfb92638e4d8f31dbadca029b1
mtk57 <mtk.kawa@gmail.com>
2025-05-30 20:39:10 +0900

    a

---
+++ .NET/SheetMergeTool/MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SheetMergeTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtStartCell = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtDirPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEndCell = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkEnableSubDir = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(22, 280);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(53, 12);
+            this.lblStatus.TabIndex = 17;
+            this.lblStatus.Text = "準備完了";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(22, 152);
+            this.txtResults.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(521, 70);
+            this.txtResults.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "処理結果";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(239, 239);
+            this.btnProcess.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(64, 24);
+            this.btnProcess.TabIndex = 14;
+            this.btnProcess.Text = "処理開始";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtStartCell
+            // 
+            this.txtStartCell.Location = new System.Drawing.Point(22, 99);
+            this.txtStartCell.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtStartCell.Name = "txtStartCell";
+            this.txtStartCell.Size = new System.Drawing.Size(132, 19);
+            this.txtStartCell.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "開始セル位置(A1形式)";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(478, 36);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(64, 18);
+            this.btnBrowse.TabIndex = 11;
+            this.btnBrowse.Text = "参照";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtDirPath
+            // 
+            this.txtDirPath.Location = new System.Drawing.Point(22, 36);
+            this.txtDirPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDirPath.Name = "txtDirPath";
+            this.txtDirPath.Size = new System.Drawing.Size(451, 19);
+            this.txtDirPath.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Excelフォルダパス";
+            // 
+            // txtEndCell
+            // 
+            this.txtEndCell.Location = new System.Drawing.Point(183, 99);
+            this.txtEndCell.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEndCell.Name = "txtEndCell";
+            this.txtEndCell.Size = new System.Drawing.Size(132, 19);
+            this.txtEndCell.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 12);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "終了セル位置(A1形式)";
+            // 
+            // chkEnableSubDir
+            // 
+            this.chkEnableSubDir.AutoSize = true;
+            this.chkEnableSubDir.Location = new System.Drawing.Point(348, 101);
+            this.chkEnableSubDir.Name = "chkEnableSubDir";
+            this.chkEnableSubDir.Size = new System.Drawing.Size(118, 16);
+            this.chkEnableSubDir.TabIndex = 20;
+            this.chkEnableSubDir.Text = "サブフォルダも含める";
+            this.chkEnableSubDir.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 321);
+            this.Controls.Add(this.chkEnableSubDir);
+            this.Controls.Add(this.txtEndCell);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtStartCell);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDirPath);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Sheet Merge Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtStartCell;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtDirPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEndCell;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkEnableSubDir;
+    }
+}
---
+++ .NET/SheetMergeTool/MainForm.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace SheetMergeTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            lblStatus.Text = "準備完了"; // Initial status [from MainForm.Designer.cs]
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Excelファイルが含まれるフォルダを選択してください";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtDirPath.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private async void btnProcess_Click(object sender, EventArgs e)
+        {
+            string folderPath = txtDirPath.Text;
+            string startCellRef = txtStartCell.Text.Trim().ToUpper();
+            string endCellRef = txtEndCell.Text.Trim().ToUpper();
+
+            if (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
+            {
+                MessageBox.Show("有効なExcelフォルダパスを指定してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!IsValidCellReference(startCellRef)) // [from MainForm.Designer.cs] for txtStartCell
+            {
+                MessageBox.Show("有効な開始セル位置を指定してください (例: A1)。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!IsValidCellReference(endCellRef)) // [from MainForm.Designer.cs] for txtEndCell
+            {
+                MessageBox.Show("有効な終了セル位置を指定してください (例: D1)。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SetControlsEnabled(false);
+            lblStatus.Text = "処理中..."; // [from MainForm.Designer.cs] for lblStatus
+            txtResults.Clear(); // [from MainForm.Designer.cs] for txtResults
+            Application.DoEvents(); 
+
+            var progressLog = new Progress<string>(message => {
+                if (txtResults.IsDisposed) return;
+                txtResults.AppendText(message + Environment.NewLine);
+            });
+            var progressStatus = new Progress<string>(status => {
+                if (lblStatus.IsDisposed) return;
+                lblStatus.Text = status;
+            });
+
+            try
+            {
+                await Task.Run(() => 
+                    MergeExcelFilesLogic(folderPath, startCellRef, endCellRef, progressLog, progressStatus)
+                );
+                ((IProgress<string>)progressStatus).Report("処理完了！");
+            }
+            catch (Exception ex)
+            {
+                ((IProgress<string>)progressLog).Report($"エラーが発生しました: {ex.Message}");
+                ((IProgress<string>)progressLog).Report($"スタックトレース: {ex.StackTrace}");
+                ((IProgress<string>)progressStatus).Report("エラー発生");
+                MessageBox.Show($"処理中にエラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                SetControlsEnabled(true);
+            }
+        }
+
+        private void SetControlsEnabled(bool enabled)
+        {
+            txtDirPath.Enabled = enabled;
+            txtStartCell.Enabled = enabled;
+            txtEndCell.Enabled = enabled;
+            btnBrowse.Enabled = enabled;
+            btnProcess.Enabled = enabled;
+        }
+
+        private void MergeExcelFilesLogic(string folderPath, string startCellUserRef, string endCellUserRef, 
+                                          IProgress<string> logger, IProgress<string> statusUpdater)
+        {
+            logger.Report("処理を開始します...");
+            logger.Report($"対象フォルダ: {folderPath}"); // [cite: 1]
+            logger.Report($"開始セル: {startCellUserRef}, 終了セル列範囲: {endCellUserRef}"); // [cite: 1]
+
+            string outputFileName = DateTime.Now.ToString("yyyyMMdd_HHmmssfff") + ".xlsx"; // [cite: 1] (YYYYMMDD_hhmmssfff.xlsx)
+            string outputFilePath = Path.Combine(folderPath, outputFileName);
+
+            GetCellRowColumn(startCellUserRef, out uint startRowIndex, out string startColumnName);
+            GetCellRowColumn(endCellUserRef, out _, out string endColumnName); 
+
+            int startColumnNum = GetColumnIndexFromName(startColumnName); 
+            int endColumnNum = GetColumnIndexFromName(endColumnName);
+
+            if (startColumnNum > endColumnNum)
+            {
+                logger.Report("警告: 開始セル列が終了セル列より後です。データ列は抽出されません。");
+            }
+            
+            statusUpdater.Report("出力ファイルを準備中...");
+
+            using (SpreadsheetDocument outputDoc = SpreadsheetDocument.Create(outputFilePath, SpreadsheetDocumentType.Workbook))
+            {
+                WorkbookPart workbookPart = outputDoc.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                SheetData sheetData = new SheetData();
+                worksheetPart.Worksheet = new Worksheet(sheetData);
+
+                Sheets sheets = outputDoc.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+                Sheet sheet = new Sheet() { Id = outputDoc.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Output" }; // [cite: 1] (シート名 [Output])
+                sheets.Append(sheet);
+
+                uint outputCurrentRow = 1;
+
+                Row headerRow = new Row() { RowIndex = outputCurrentRow };
+                headerRow.Append(CreateTextCell(GetColumnNameFromIndex(0), outputCurrentRow, "ファイルパス")); // [cite: 1]
+                headerRow.Append(CreateTextCell(GetColumnNameFromIndex(1), outputCurrentRow, "ファイル名"));   // [cite: 1]
+                headerRow.Append(CreateTextCell(GetColumnNameFromIndex(2), outputCurrentRow, "シート名"));     // [cite: 1]
+
+                int dataColOutputIdx = 0;
+                for (int col = startColumnNum; col <= endColumnNum; col++)
+                {
+                    headerRow.Append(CreateTextCell(GetColumnNameFromIndex(3 + dataColOutputIdx), outputCurrentRow, $"Data {dataColOutputIdx + 1}"));
+                    dataColOutputIdx++;
+                }
+                sheetData.Append(headerRow);
+                outputCurrentRow++;
+
+                string[] fileExtensions = { "*.xlsx", "*.xlsm" }; // [cite: 1] (対象とする。)
+                List<string> filesToProcess = new List<string>();
+                foreach (string ext in fileExtensions)
+                {
+                    filesToProcess.AddRange(Directory.GetFiles(folderPath, ext));
+                }
+
+                if (!filesToProcess.Any())
+                {
+                    logger.Report("対象フォルダに処理対象のExcelファイルが見つかりませんでした。");
+                    statusUpdater.Report("対象ファイルなし");
+                    return;
+                }
+
+                foreach (string filePath in filesToProcess)
+                {
+                    if (Path.GetFileName(filePath).Equals(outputFileName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        continue; 
+                    }
+
+                    string fileNameOnly = Path.GetFileName(filePath);
+                    logger.Report($"処理中のファイル: {fileNameOnly}");
+                    statusUpdater.Report($"処理中: {fileNameOnly}");
+                    
+                    try
+                    {
+                        using (SpreadsheetDocument sourceDoc = SpreadsheetDocument.Open(filePath, false))
+                        {
+                            WorkbookPart sourceWorkbookPart = sourceDoc.WorkbookPart;
+                            if (sourceWorkbookPart?.Workbook?.Sheets == null)
+                            {
+                                logger.Report($"警告: {fileNameOnly} のワークブック構造を読み取れませんでした。スキップします。");
+                                continue;
+                            }
+
+                            foreach (Sheet sourceSheet in sourceWorkbookPart.Workbook.Sheets.Elements<Sheet>())
+                            {
+                                if (sourceSheet?.Name == null || sourceSheet.Id?.Value == null) continue;
+                                string sheetName = sourceSheet.Name;
+                                logger.Report($"  処理中のシート: {sheetName}");
+
+                                WorksheetPart sourceWorksheetPart = (WorksheetPart)sourceWorkbookPart.GetPartById(sourceSheet.Id.Value);
+                                
+                                uint currentSourceRow = startRowIndex; 
+
+                                while (true)
+                                {
+                                    string firstCellInRowRef = startColumnName + currentSourceRow;
+                                    string firstCellValue = GetCellValue(sourceWorkbookPart, sourceWorksheetPart, firstCellInRowRef);
+
+                                    if (string.IsNullOrEmpty(firstCellValue)) // [cite: 1] (空行を検知するまでの行をOutputシートにコピーする。)
+                                    {
+                                        logger.Report($"    シート '{sheetName}' の行 {currentSourceRow} ({startColumnName}{currentSourceRow}) で空のセルを検出。このシートの処理を終了します。");
+                                        break; 
+                                    }
+
+                                    Row dataRow = new Row() { RowIndex = outputCurrentRow };
+                                    dataRow.Append(CreateTextCell(GetColumnNameFromIndex(0), outputCurrentRow, filePath));
+                                    dataRow.Append(CreateTextCell(GetColumnNameFromIndex(1), outputCurrentRow, fileNameOnly));
+                                    dataRow.Append(CreateTextCell(GetColumnNameFromIndex(2), outputCurrentRow, sheetName));
+
+                                    int outputDataColLetterIdx = 3; 
+                                    for (int colIdx = startColumnNum; colIdx <= endColumnNum; colIdx++)
+                                    {
+                                        string currentSourceColName = GetColumnNameFromIndex(colIdx);
+                                        string cellRef = currentSourceColName + currentSourceRow;
+                                        string cellValueStr = GetCellValue(sourceWorkbookPart, sourceWorksheetPart, cellRef);
+                                        dataRow.Append(CreateTextCell(GetColumnNameFromIndex(outputDataColLetterIdx++), outputCurrentRow, cellValueStr ?? string.Empty));
+                                    }
+
+                                    sheetData.Append(dataRow);
+                                    outputCurrentRow++;
+                                    currentSourceRow++;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                         logger.Report($"ファイル処理エラー {fileNameOnly}: {ex.Message}。このファイルをスキップします。");
+                    }
+                }
+                workbookPart.Workbook.Save();
+                logger.Report($"処理完了。出力ファイル: {outputFilePath}");
+                // Final status update is handled in btnProcess_Click's main try-catch
+            }
+        }
+
+        private static Cell CreateTextCell(string columnLetter, uint rowIndex, string text)
+        {
+            return new Cell(new CellValue(text))
+            {
+                CellReference = columnLetter + rowIndex,
+                DataType = CellValues.String 
+            };
+        }
+
+        private static string GetCellValue(WorkbookPart workbookPart, WorksheetPart worksheetPart, string cellAddress)
+        {
+            Cell theCell = worksheetPart.Worksheet.Descendants<Cell>().FirstOrDefault(c => c.CellReference?.Value == cellAddress);
+
+            if (theCell == null || theCell.CellValue == null)
+            {
+                return null; 
+            }
+
+            string value = theCell.CellValue.InnerText;
+            if (theCell.DataType != null && theCell.DataType.Value == CellValues.SharedString)
+            {
+                SharedStringTablePart sstPart = workbookPart?.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                if (sstPart != null && int.TryParse(value, out int sstId))
+                {
+                    if (sstId >= 0 && sstId < sstPart.SharedStringTable.ChildElements.Count)
+                    {
+                         return sstPart.SharedStringTable.ChildElements[sstId].InnerText;
+                    }
+                    return $"#SST_ID_ERR({sstId})"; 
+                }
+                else if (sstPart == null && theCell.DataType.Value == CellValues.SharedString)
+                {
+                    return value; 
+                }
+                return value; 
+            }
+            return value;
+        }
+        
+        public static int GetColumnIndexFromName(string columnName)
+        {
+            if (string.IsNullOrEmpty(columnName)) throw new ArgumentNullException(nameof(columnName));
+            int index = 0;
+            columnName = columnName.ToUpper();
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                index *= 26;
+                index += (columnName[i] - 'A' + 1);
+            }
+            return index - 1; 
+        }
+
+        public static string GetColumnNameFromIndex(int columnIndex)
+        {
+            int dividend = columnIndex + 1; 
+            string columnName = String.Empty;
+            while (dividend > 0)
+            {
+                int modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo) + columnName;
+                dividend = (dividend - modulo) / 26;
+            }
+            return columnName;
+        }
+        
+        public static void GetCellRowColumn(string cellReference, out uint rowIndex, out string columnName)
+        {
+            if (string.IsNullOrEmpty(cellReference)) throw new ArgumentNullException(nameof(cellReference));
+            cellReference = cellReference.ToUpper();
+
+            Match match = Regex.Match(cellReference, @"([A-Z]+)(\d+)");
+            if (!match.Success) throw new ArgumentException("Invalid cell reference format.", nameof(cellReference));
+
+            columnName = match.Groups[1].Value;
+            rowIndex = uint.Parse(match.Groups[2].Value);
+            if (rowIndex == 0) throw new ArgumentException("Row index cannot be 0.", nameof(cellReference));
+        }
+        
+        public static bool IsValidCellReference(string cellRef)
+        {
+            if (string.IsNullOrWhiteSpace(cellRef)) return false;
+            return Regex.IsMatch(cellRef.ToUpper(), @"^[A-Z]+[1-9][0-9]*$");
+        }
+    }
+}

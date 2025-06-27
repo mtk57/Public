2525fe035117894f2e79e06a718c23761d5476e3
mtk57 <mtk.kawa@gmail.com>
2025-06-27 22:46:32 +0900

    0.0.1

---
+++ .NET/CursorMover/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CursorMover
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textCursorMoveTimeSec = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textCursorMoveTimeSec
+            // 
+            this.textCursorMoveTimeSec.Location = new System.Drawing.Point(29, 50);
+            this.textCursorMoveTimeSec.Name = "textCursorMoveTimeSec";
+            this.textCursorMoveTimeSec.Size = new System.Drawing.Size(182, 19);
+            this.textCursorMoveTimeSec.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "カーソル自動移動間隔（秒）";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(29, 107);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(79, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(132, 107);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(79, 25);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 158);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textCursorMoveTimeSec);
+            this.Name = "MainForm";
+            this.Text = "Cursor Mover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textCursorMoveTimeSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+

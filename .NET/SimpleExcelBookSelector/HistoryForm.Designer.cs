85db453f4ae10a23ee21eb716a9d09db008636fc
mtk57 <mtk.kawa@gmail.com>
2025-09-23 09:17:45 +0900

    btnOpenAllPin add

--- .NET/SimpleExcelBookSelector/HistoryForm.Designer.cs
+++ .NET/SimpleExcelBookSelector/HistoryForm.Designer.cs
@@ -41,6 +41,7 @@
             this.btnUnPinnedSelectedFiles = new System.Windows.Forms.Button();
             this.btnApply = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOpenAllPin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,13 +51,13 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
             this.dataGridView1.Location = new System.Drawing.Point(21, 66);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(691, 127);
+            this.dataGridView1.Size = new System.Drawing.Size(775, 127);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
             // 
             // btnAllOpen
             // 
@@ -70,7 +71,7 @@
             // 
             // btnAllClear
             // 
-            this.btnAllClear.Location = new System.Drawing.Point(391, 12);
+            this.btnAllClear.Location = new System.Drawing.Point(493, 12);
             this.btnAllClear.Name = "btnAllClear";
             this.btnAllClear.Size = new System.Drawing.Size(119, 23);
             this.btnAllClear.TabIndex = 2;
@@ -80,7 +81,7 @@
             // 
             // btnSelectOpen
             // 
-            this.btnSelectOpen.Location = new System.Drawing.Point(102, 12);
+            this.btnSelectOpen.Location = new System.Drawing.Point(205, 12);
             this.btnSelectOpen.Name = "btnSelectOpen";
             this.btnSelectOpen.Size = new System.Drawing.Size(121, 23);
             this.btnSelectOpen.TabIndex = 3;
@@ -90,7 +91,7 @@
             // 
             // btnDeleteSelectedFiles
             // 
-            this.btnDeleteSelectedFiles.Location = new System.Drawing.Point(516, 12);
+            this.btnDeleteSelectedFiles.Location = new System.Drawing.Point(618, 12);
             this.btnDeleteSelectedFiles.Name = "btnDeleteSelectedFiles";
             this.btnDeleteSelectedFiles.Size = new System.Drawing.Size(121, 23);
             this.btnDeleteSelectedFiles.TabIndex = 4;
@@ -100,7 +101,7 @@
             // 
             // btnUnselectedAll
             // 
-            this.btnUnselectedAll.Location = new System.Drawing.Point(229, 12);
+            this.btnUnselectedAll.Location = new System.Drawing.Point(332, 12);
             this.btnUnselectedAll.Name = "btnUnselectedAll";
             this.btnUnselectedAll.Size = new System.Drawing.Size(99, 23);
             this.btnUnselectedAll.TabIndex = 5;
@@ -110,16 +111,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 41);
+            this.textBox1.Location = new System.Drawing.Point(63, 39);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(265, 19);
+            this.textBox1.Size = new System.Drawing.Size(368, 19);
             this.textBox1.TabIndex = 6;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 44);
+            this.label1.Location = new System.Drawing.Point(19, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 12);
             this.label1.TabIndex = 7;
@@ -127,7 +128,7 @@
             // 
             // btnClearFilter
             // 
-            this.btnClearFilter.Location = new System.Drawing.Point(335, 37);
+            this.btnClearFilter.Location = new System.Drawing.Point(437, 37);
             this.btnClearFilter.Name = "btnClearFilter";
             this.btnClearFilter.Size = new System.Drawing.Size(48, 23);
             this.btnClearFilter.TabIndex = 8;
@@ -137,7 +138,7 @@
             // 
             // btnPinnedSelectedFiles
             // 
-            this.btnPinnedSelectedFiles.Location = new System.Drawing.Point(391, 37);
+            this.btnPinnedSelectedFiles.Location = new System.Drawing.Point(493, 37);
             this.btnPinnedSelectedFiles.Name = "btnPinnedSelectedFiles";
             this.btnPinnedSelectedFiles.Size = new System.Drawing.Size(137, 23);
             this.btnPinnedSelectedFiles.TabIndex = 9;
@@ -147,7 +148,7 @@
             // 
             // btnUnPinnedSelectedFiles
             // 
-            this.btnUnPinnedSelectedFiles.Location = new System.Drawing.Point(534, 37);
+            this.btnUnPinnedSelectedFiles.Location = new System.Drawing.Point(636, 37);
             this.btnUnPinnedSelectedFiles.Name = "btnUnPinnedSelectedFiles";
             this.btnUnPinnedSelectedFiles.Size = new System.Drawing.Size(158, 23);
             this.btnUnPinnedSelectedFiles.TabIndex = 10;
@@ -159,7 +160,7 @@
             // 
             this.btnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnApply.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnApply.Location = new System.Drawing.Point(608, 207);
+            this.btnApply.Location = new System.Drawing.Point(692, 207);
             this.btnApply.Name = "btnApply";
             this.btnApply.Size = new System.Drawing.Size(49, 23);
             this.btnApply.TabIndex = 11;
@@ -170,18 +171,28 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(663, 207);
+            this.btnCancel.Location = new System.Drawing.Point(747, 207);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(49, 23);
             this.btnCancel.TabIndex = 12;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
+            // btnOpenAllPin
+            // 
+            this.btnOpenAllPin.Location = new System.Drawing.Point(102, 12);
+            this.btnOpenAllPin.Name = "btnOpenAllPin";
+            this.btnOpenAllPin.Size = new System.Drawing.Size(103, 23);
+            this.btnOpenAllPin.TabIndex = 13;
+            this.btnOpenAllPin.Text = "ピン止めを全て開く";
+            this.btnOpenAllPin.UseVisualStyleBackColor = true;
+            // 
             // HistoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 242);
+            this.ClientSize = new System.Drawing.Size(808, 242);
+            this.Controls.Add(this.btnOpenAllPin);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnApply);
             this.Controls.Add(this.btnUnPinnedSelectedFiles);
@@ -219,5 +230,6 @@
         private System.Windows.Forms.Button btnUnPinnedSelectedFiles;
         private System.Windows.Forms.Button btnApply;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOpenAllPin;
     }
 }

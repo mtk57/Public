9d87c90264ca8635ad93334952b58f650ad7d2a7
mtk57 <mtk.kawa@gmail.com>
2025-10-28 20:33:22 +0900

    ver1.3.11 未実装

--- .NET/SimpleExcelBookSelector/HistoryForm.Designer.cs
+++ .NET/SimpleExcelBookSelector/HistoryForm.Designer.cs
@@ -46,6 +46,14 @@
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnOpenAllPin = new System.Windows.Forms.Button();
             this.chkIsOpenDir = new System.Windows.Forms.CheckBox();
+            this.txtDirFilter = new System.Windows.Forms.TextBox();
+            this.txtFileFilter = new System.Windows.Forms.TextBox();
+            this.txtFilePathFilter = new System.Windows.Forms.TextBox();
+            this.txtUpdateTimeFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,10 +72,10 @@
             this.colFileName,
             this.colFilePath,
             this.colUpdatedAt});
-            this.dataGridView1.Location = new System.Drawing.Point(21, 71);
+            this.dataGridView1.Location = new System.Drawing.Point(21, 126);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(775, 208);
+            this.dataGridView1.Size = new System.Drawing.Size(775, 153);
             this.dataGridView1.TabIndex = 10;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
@@ -228,11 +236,83 @@
             this.chkIsOpenDir.Text = "ダブルクリックでフォルダを開く";
             this.chkIsOpenDir.UseVisualStyleBackColor = true;
             // 
+            // txtDirFilter
+            // 
+            this.txtDirFilter.Location = new System.Drawing.Point(21, 101);
+            this.txtDirFilter.Name = "txtDirFilter";
+            this.txtDirFilter.Size = new System.Drawing.Size(75, 19);
+            this.txtDirFilter.TabIndex = 15;
+            // 
+            // txtFileFilter
+            // 
+            this.txtFileFilter.Location = new System.Drawing.Point(102, 101);
+            this.txtFileFilter.Name = "txtFileFilter";
+            this.txtFileFilter.Size = new System.Drawing.Size(75, 19);
+            this.txtFileFilter.TabIndex = 16;
+            // 
+            // txtFilePathFilter
+            // 
+            this.txtFilePathFilter.Location = new System.Drawing.Point(183, 101);
+            this.txtFilePathFilter.Name = "txtFilePathFilter";
+            this.txtFilePathFilter.Size = new System.Drawing.Size(75, 19);
+            this.txtFilePathFilter.TabIndex = 17;
+            // 
+            // txtUpdateTimeFilter
+            // 
+            this.txtUpdateTimeFilter.Location = new System.Drawing.Point(264, 101);
+            this.txtUpdateTimeFilter.Name = "txtUpdateTimeFilter";
+            this.txtUpdateTimeFilter.Size = new System.Drawing.Size(75, 19);
+            this.txtUpdateTimeFilter.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "フォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 12);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "ファイル";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 12);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "ファイルパス";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "更新日時";
+            // 
             // HistoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(808, 320);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUpdateTimeFilter);
+            this.Controls.Add(this.txtFilePathFilter);
+            this.Controls.Add(this.txtFileFilter);
+            this.Controls.Add(this.txtDirFilter);
             this.Controls.Add(this.chkIsOpenDir);
             this.Controls.Add(this.btnOpenAllPin);
             this.Controls.Add(this.btnCancel);
@@ -274,5 +354,13 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colFilePath;
         private System.Windows.Forms.DataGridViewTextBoxColumn colUpdatedAt;
         private System.Windows.Forms.CheckBox chkIsOpenDir;
+        private System.Windows.Forms.TextBox txtDirFilter;
+        private System.Windows.Forms.TextBox txtFileFilter;
+        private System.Windows.Forms.TextBox txtFilePathFilter;
+        private System.Windows.Forms.TextBox txtUpdateTimeFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }

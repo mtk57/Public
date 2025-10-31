ff77c10eda869b90bcbf23bd81487cc8829764a8
mtk57 <mtk.kawa@gmail.com>
2025-11-01 08:25:29 +0900

    a

--- sakura/sakura_tag_jump.js
+++ sakura/sakura_tag_jump.js
@@ -1,16 +1,26 @@
 var EXE_PATH = "C:\\_git\\Public\\.NET\\SimpleMethodCallListCreator\\bin\\Debug\\SimpleMethodCallListCreator.exe";
 
-// 現在行の文字列を取得
-var lineText = GetLineStr(0);
+// exeファイルの存在チェック
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var shell = new ActiveXObject("WScript.Shell");
 
-// 末尾の改行文字を削除
-lineText = lineText.replace(/\r?\n$/, '');
+if (!fso.FileExists(EXE_PATH)) {
+    shell.Popup("実行ファイルが見つかりません:\n" + EXE_PATH, 0, "エラー", 16);
+} else {
+    // 以降の処理...
 
-// ダブルクォーテーションをエスケープ（"を\"に変換）
-lineText = lineText.replace(/"/g, '\\"');
+    // 現在行の文字列を取得
+    var lineText = GetLineStr(0);
 
-// コマンドライン引数として渡す（ダブルクォートで囲む）
-var args = '"' + lineText + '"'
+    // 末尾の改行文字を削除
+    lineText = lineText.replace(/\r?\n$/, '');
 
-// EXEを実行
-ExecCommand('"' + EXE_PATH + '" ' + args);
+    // ダブルクォーテーションをエスケープ（"を\"に変換）
+    lineText = lineText.replace(/"/g, '\\"');
+
+    // コマンドライン引数として渡す（ダブルクォートで囲む）
+    var args = '"' + lineText + '"'
+
+    // EXEを実行
+    ExecCommand('"' + EXE_PATH + '" ' + args);
+}

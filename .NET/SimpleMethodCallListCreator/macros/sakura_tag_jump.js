a3961801c48c82d8520cbc6fa08baae7e4f9f75c
mtk57 <mtk.kawa@gmail.com>
2025-11-01 07:10:57 +0900

    マクロにメソッドリストは含めないように修正

--- .NET/SimpleMethodCallListCreator/macros/sakura_tag_jump.js
+++ .NET/SimpleMethodCallListCreator/macros/sakura_tag_jump.js
@@ -1,5 +1,4 @@
 var EXE_PATH = "C:\\_git\\Public\\.NET\\SimpleMethodCallListCreator\\bin\\Debug\\SimpleMethodCallListCreator.exe";
-var METHOD_LIST_PATH = "C:\\_git\\Public\\.NET\\SimpleMethodCallListCreator\\expectdata\\methodlist_tagjump.tsv";
 
 // 現在行の文字列を取得
 var lineText = GetLineStr(0);
@@ -10,11 +9,8 @@ lineText = lineText.replace(/\r?\n$/, '');
 // ダブルクォーテーションをエスケープ（"を\"に変換）
 lineText = lineText.replace(/"/g, '\\"');
 
-// METHOD_LIST_PATHもエスケープ（念のため）
-var escapedMethodListPath = METHOD_LIST_PATH.replace(/"/g, '\\"');
-
 // コマンドライン引数として渡す（ダブルクォートで囲む）
-var args = '"' + lineText + '"' + ' ' + '"' + escapedMethodListPath + '"';
+var args = '"' + lineText + '"'
 
 // EXEを実行
 ExecCommand('"' + EXE_PATH + '" ' + args);

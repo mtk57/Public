66b1416173e174e46a8ba5d173b4e4c0dc4fb03e
mtk57 <mtk.kawa@gmail.com>
2026-02-14 07:01:28 +0900

    ver1.1.0 未テスト

--- .NET/CursorMover/Form1.Designer.cs
+++ .NET/CursorMover/Form1.Designer.cs
@@ -34,19 +34,27 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblElapsedTime = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtElapsedTimeBefore = new System.Windows.Forms.TextBox();
+            this.btnSaveElapsedTimeBefore = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMemo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textCursorMoveTimeSec
             // 
-            this.textCursorMoveTimeSec.Location = new System.Drawing.Point(29, 50);
+            this.textCursorMoveTimeSec.Location = new System.Drawing.Point(173, 16);
             this.textCursorMoveTimeSec.Name = "textCursorMoveTimeSec";
-            this.textCursorMoveTimeSec.Size = new System.Drawing.Size(182, 19);
+            this.textCursorMoveTimeSec.Size = new System.Drawing.Size(55, 19);
             this.textCursorMoveTimeSec.TabIndex = 1;
+            this.textCursorMoveTimeSec.Text = "5";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Location = new System.Drawing.Point(12, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(139, 12);
             this.label1.TabIndex = 0;
@@ -54,7 +62,7 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(29, 107);
+            this.btnStart.Location = new System.Drawing.Point(45, 163);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(79, 25);
             this.btnStart.TabIndex = 2;
@@ -64,18 +72,85 @@
             // btnStop
             // 
             this.btnStop.Enabled = false;
-            this.btnStop.Location = new System.Drawing.Point(132, 107);
+            this.btnStop.Location = new System.Drawing.Point(148, 163);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(79, 25);
             this.btnStop.TabIndex = 3;
             this.btnStop.Text = "STOP";
             this.btnStop.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "経過時間";
+            // 
+            // lblElapsedTime
+            // 
+            this.lblElapsedTime.AutoSize = true;
+            this.lblElapsedTime.Location = new System.Drawing.Point(119, 60);
+            this.lblElapsedTime.Name = "lblElapsedTime";
+            this.lblElapsedTime.Size = new System.Drawing.Size(55, 12);
+            this.lblElapsedTime.TabIndex = 5;
+            this.lblElapsedTime.Text = "HH:mm:ss";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "経過時間（前回）";
+            // 
+            // txtElapsedTimeBefore
+            // 
+            this.txtElapsedTimeBefore.Location = new System.Drawing.Point(121, 88);
+            this.txtElapsedTimeBefore.Name = "txtElapsedTimeBefore";
+            this.txtElapsedTimeBefore.Size = new System.Drawing.Size(78, 19);
+            this.txtElapsedTimeBefore.TabIndex = 7;
+            // 
+            // btnSaveElapsedTimeBefore
+            // 
+            this.btnSaveElapsedTimeBefore.Enabled = false;
+            this.btnSaveElapsedTimeBefore.Location = new System.Drawing.Point(216, 88);
+            this.btnSaveElapsedTimeBefore.Name = "btnSaveElapsedTimeBefore";
+            this.btnSaveElapsedTimeBefore.Size = new System.Drawing.Size(44, 19);
+            this.btnSaveElapsedTimeBefore.TabIndex = 8;
+            this.btnSaveElapsedTimeBefore.Text = "保持";
+            this.btnSaveElapsedTimeBefore.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "メモ";
+            // 
+            // txtMemo
+            // 
+            this.txtMemo.Location = new System.Drawing.Point(121, 119);
+            this.txtMemo.Name = "txtMemo";
+            this.txtMemo.Size = new System.Drawing.Size(139, 19);
+            this.txtMemo.TabIndex = 10;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 158);
+            this.ClientSize = new System.Drawing.Size(292, 200);
+            this.Controls.Add(this.txtMemo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSaveElapsedTimeBefore);
+            this.Controls.Add(this.txtElapsedTimeBefore);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblElapsedTime);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label1);
@@ -95,5 +170,12 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblElapsedTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtElapsedTimeBefore;
+        private System.Windows.Forms.Button btnSaveElapsedTimeBefore;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMemo;
     }
 }
--- .NET/CursorMover/Form1.cs
+++ .NET/CursorMover/Form1.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices; // DllImportに必要
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace CursorMover
@@ -10,6 +16,10 @@ namespace CursorMover
     {
         // 次にマウスカーソルを右に動かすかどうかのフラグ
         private bool moveRight = true;
+        private readonly Stopwatch elapsedStopwatch = new Stopwatch();
+        private string elapsedTimeBeforeTextBeforeEdit = string.Empty;
+        private readonly string settingsFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CursorMoverSettings.json");
+        private static readonly Regex ElapsedTimePattern = new Regex(@"^\d{2,}:[0-5]\d:[0-5]\d$", RegexOptions.Compiled);
 
         public MainForm()
         {
@@ -21,7 +31,11 @@ namespace CursorMover
             // イベントハンドラを登録
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnSaveElapsedTimeBefore.Click += new System.EventHandler(this.btnSaveElapsedTimeBefore_Click);
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.txtElapsedTimeBefore.Enter += new System.EventHandler(this.txtElapsedTimeBefore_Enter);
+            this.txtElapsedTimeBefore.Leave += new System.EventHandler(this.txtElapsedTimeBefore_Leave);
+            this.FormClosing += new FormClosingEventHandler(this.MainForm_FormClosing);
         }
 
         /// <summary>
@@ -37,10 +51,13 @@ namespace CursorMover
                 timer1.Interval = intervalSeconds * 1000;
                 // タイマーを開始
                 timer1.Start();
+                elapsedStopwatch.Restart();
+                lblElapsedTime.Text = "00:00:00";
 
                 // UIの状態を更新
                 btnStart.Enabled = false;
                 btnStop.Enabled = true;
+                btnSaveElapsedTimeBefore.Enabled = false;
                 textCursorMoveTimeSec.ReadOnly = true;
             }
             else
@@ -57,10 +74,13 @@ namespace CursorMover
         {
             // タイマーを停止
             timer1.Stop();
+            elapsedStopwatch.Stop();
+            lblElapsedTime.Text = FormatElapsedTime(elapsedStopwatch.Elapsed);
 
             // UIの状態を更新
             btnStart.Enabled = true;
             btnStop.Enabled = false;
+            btnSaveElapsedTimeBefore.Enabled = true;
             textCursorMoveTimeSec.ReadOnly = false;
         }
 
@@ -148,6 +168,140 @@ namespace CursorMover
             // タイトルにバージョン情報を表示
             var version = Assembly.GetExecutingAssembly().GetName().Version;
             this.Text = $"{this.Text}  ver {version.Major}.{version.Minor}.{version.Build}";
+
+            // 画面初期状態を設定
+            btnSaveElapsedTimeBefore.Enabled = false;
+            lblElapsedTime.Text = "00:00:00";
+
+            // 前回終了時のUI状態を復元
+            LoadUiStateFromJson();
+            elapsedTimeBeforeTextBeforeEdit = txtElapsedTimeBefore.Text;
         }
+
+        private void btnSaveElapsedTimeBefore_Click(object sender, EventArgs e)
+        {
+            txtElapsedTimeBefore.Text = lblElapsedTime.Text;
+            elapsedTimeBeforeTextBeforeEdit = txtElapsedTimeBefore.Text;
+        }
+
+        private void txtElapsedTimeBefore_Enter(object sender, EventArgs e)
+        {
+            elapsedTimeBeforeTextBeforeEdit = txtElapsedTimeBefore.Text;
+        }
+
+        private void txtElapsedTimeBefore_Leave(object sender, EventArgs e)
+        {
+            string currentText = txtElapsedTimeBefore.Text.Trim();
+
+            // 値が変わっていない場合は何もしない
+            if (string.Equals(currentText, elapsedTimeBeforeTextBeforeEdit, StringComparison.Ordinal))
+            {
+                return;
+            }
+
+            if (!IsValidElapsedTimeText(currentText))
+            {
+                txtElapsedTimeBefore.Text = elapsedTimeBeforeTextBeforeEdit;
+                MessageBox.Show("経過時間（前回）は HH:mm:ss 形式で入力してください。", "入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtElapsedTimeBefore.Text = currentText;
+            elapsedTimeBeforeTextBeforeEdit = currentText;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveUiStateToJson();
+        }
+
+        private static string FormatElapsedTime(TimeSpan elapsed)
+        {
+            int totalHours = (int)elapsed.TotalHours;
+            return string.Format(CultureInfo.InvariantCulture, "{0:D2}:{1:D2}:{2:D2}", totalHours, elapsed.Minutes, elapsed.Seconds);
+        }
+
+        private static bool IsValidElapsedTimeText(string value)
+        {
+            return ElapsedTimePattern.IsMatch(value ?? string.Empty);
+        }
+
+        private void LoadUiStateFromJson()
+        {
+            if (!File.Exists(settingsFilePath))
+            {
+                return;
+            }
+
+            try
+            {
+                string json = File.ReadAllText(settingsFilePath, Encoding.UTF8);
+                var serializer = new JavaScriptSerializer();
+                UiState uiState = serializer.Deserialize<UiState>(json);
+
+                if (uiState == null)
+                {
+                    return;
+                }
+
+                if (!string.IsNullOrWhiteSpace(uiState.CursorMoveIntervalSeconds))
+                {
+                    textCursorMoveTimeSec.Text = uiState.CursorMoveIntervalSeconds.Trim();
+                }
+
+                if (!string.IsNullOrWhiteSpace(uiState.ElapsedTimeBefore))
+                {
+                    string elapsedText = uiState.ElapsedTimeBefore.Trim();
+                    if (IsValidElapsedTimeText(elapsedText))
+                    {
+                        txtElapsedTimeBefore.Text = elapsedText;
+                    }
+                }
+
+                if (uiState.Memo != null)
+                {
+                    txtMemo.Text = uiState.Memo;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"設定ファイルの読み込みに失敗しました。\n{ex.Message}", "設定読込エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void SaveUiStateToJson()
+        {
+            try
+            {
+                string elapsedTimeBefore = txtElapsedTimeBefore.Text?.Trim() ?? string.Empty;
+                if (!IsValidElapsedTimeText(elapsedTimeBefore))
+                {
+                    elapsedTimeBefore = elapsedTimeBeforeTextBeforeEdit;
+                }
+
+                var uiState = new UiState
+                {
+                    CursorMoveIntervalSeconds = textCursorMoveTimeSec.Text?.Trim(),
+                    ElapsedTimeBefore = elapsedTimeBefore,
+                    Memo = txtMemo.Text ?? string.Empty
+                };
+
+                var serializer = new JavaScriptSerializer();
+                string json = serializer.Serialize(uiState);
+                File.WriteAllText(settingsFilePath, json, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"設定ファイルの保存に失敗しました。\n{ex.Message}", "設定保存エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private sealed class UiState
+        {
+            public string CursorMoveIntervalSeconds { get; set; }
+            public string ElapsedTimeBefore { get; set; }
+            public string Memo { get; set; }
+        }
+
     }
 }

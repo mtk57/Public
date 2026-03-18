b848f389897d98c1c82cc6f9d16bd48a40973a8f
mtk57 <mtk.kawa@gmail.com>
2026-03-18 20:43:58 +0900

    v1.3.0

---
+++ .NET/Dir2Txt/DivideForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Dir2Txt
+{
+    partial class DivideForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(12, 29);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(153, 59);
+            this.btnCopyToClipboard.TabIndex = 0;
+            this.btnCopyToClipboard.Text = "クリップボードにコピー";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(13, 11);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 12);
+            this.lblCount.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(15, 116);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(150, 22);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "閉じる";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // DivideForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(183, 156);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnCopyToClipboard);
+            this.Name = "DivideForm";
+            this.Text = "DivideForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ .NET/Dir2Txt/DivideForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dir2Txt
+{
+    public partial class DivideForm : Form
+    {
+        public DivideForm ()
+        {
+            InitializeComponent();
+        }
+    }
+}

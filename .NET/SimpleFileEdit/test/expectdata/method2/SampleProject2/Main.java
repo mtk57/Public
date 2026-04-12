f3b0a936d9d62490f1ba87702ffdfa9f7a3e7da1
mtk57 <mtk.kawa@gmail.com>
2026-04-12 16:01:02 +0900

    期待結果を追加

---
+++ .NET/SimpleFileEdit/test/expectdata/method2/SampleProject2/Main.java
@@ -0,0 +1,11 @@
+package sample;
+
+// ファイル冒頭のコメント
+public class Main {
+    
+    public int deleteFunc() {  //★DEL
+    }
+
+    public int deleteFunc2() {  //★DEL
+    }
+}
---
+++ .NET/SimpleFileEdit/test/expectdata/method2/SampleProject2/sub/Utils.java
@@ -0,0 +1,10 @@
+package sample.sub;
+
+public class Utils {
+	
+    public static int deleteFunc() {  //šDEL
+    }
+
+    public static int deleteFunc2() {  //šDEL
+    }
+}

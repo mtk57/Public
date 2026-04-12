be42c8cf2024fdaa61575457043a4761d67be20c
mtk57 <mtk.kawa@gmail.com>
2026-04-12 15:52:36 +0900

    期待結果を追加

---
+++ .NET/SimpleFileEdit/test/expectdata/method1/SampleProject2/sub/Utils.java
@@ -0,0 +1,17 @@
+package sample.sub;
+
+public class Utils {
+	
+    public static int add(int a, int b) {   // ‘«‚µZ
+        return a + b; /* method end */
+    }
+    
+    public static String wrap(String text) {
+        
+        return "[" + text + "]";  // ‘•ü
+    }
+
+    private static int nonDeleteFunc() {
+    }
+
+}

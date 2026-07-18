1c3190c128f36568dbcddfed862a5fd360ed5c9d
mtk57 <mtk.kawa@gmail.com>
2026-07-19 06:55:50 +0900

    a

--- .NET/SimpleFileEdit/test/expectdata/empty/SampleProject/sub/Utils.java
+++
@@ -1,9 +0,0 @@
-package sample.sub;
-public class Utils {
-    public static int add(int a, int b) {   // ‘«‚µZ
-        return a + b; /* method end */
-    }
-    public static String wrap(String text) {
-        return "[" + text + "]";  // ‘•ü
-    }
-}
